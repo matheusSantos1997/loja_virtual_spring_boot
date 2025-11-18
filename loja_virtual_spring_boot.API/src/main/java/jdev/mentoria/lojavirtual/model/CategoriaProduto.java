6934be88b269b3a807a58ed2fb8287c05736fd8c
Matheus <matheus.uspeu@gmail.com>
2025-11-18 18:47:31 -0300

    updated entities MarcaProduto e CategoriaProduto

--- loja_virtual_spring_boot.API/src/main/java/jdev/mentoria/lojavirtual/model/CategoriaProduto.java
+++ loja_virtual_spring_boot.API/src/main/java/jdev/mentoria/lojavirtual/model/CategoriaProduto.java
@@ -1,8 +1,14 @@
 package jdev.mentoria.lojavirtual.model;
 
-import jakarta.persistence.*;
-
 import java.util.Objects;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "categoria_produto")
