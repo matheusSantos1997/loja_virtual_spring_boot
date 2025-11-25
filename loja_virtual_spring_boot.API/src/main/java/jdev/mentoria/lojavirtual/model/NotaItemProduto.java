a1c64da07017d61d4530b895d55f43a868f2c8b0
Matheus <matheus.uspeu@gmail.com>
2025-11-25 17:17:12 -0300

    criando entidade notaitemproduto

---
+++ loja_virtual_spring_boot.API/src/main/java/jdev/mentoria/lojavirtual/model/NotaItemProduto.java
@@ -0,0 +1,83 @@
+package jdev.mentoria.lojavirtual.model;
+
+import java.util.Objects;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.ConstraintMode;
+import jakarta.persistence.Entity;
+import jakarta.persistence.ForeignKey;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "nota_item_produto")
+@SequenceGenerator(name = "seq_nota_item_produto", sequenceName = "seq_nota_item_produto", allocationSize = 1, initialValue = 1)
+public class NotaItemProduto {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_nota_item_produto")
+    private Long id;
+    
+    @Column(nullable = false)
+    private Double quantidade;
+    
+    @ManyToOne
+    @JoinColumn(name = "nota_fiscal_compra_id", nullable = false,
+    foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "nota_fiscal_compra_fk"))
+    private NotaFiscalCompra notaFiscalCompra;
+
+    @ManyToOne
+    @JoinColumn(name = "produto_id", nullable = false,
+    foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "produto_fk"))
+    private Produto produto;
+
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Double getQuantidade() {
+        return this.quantidade;
+    }
+
+    public void setQuantidade(Double quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public NotaFiscalCompra getNotaFiscalCompra() {
+        return this.notaFiscalCompra;
+    }
+
+    public void setNotaFiscalCompra(NotaFiscalCompra notaFiscalCompra) {
+        this.notaFiscalCompra = notaFiscalCompra;
+    }
+
+    public Produto getProduto() {
+        return this.produto;
+    }
+
+    public void setProduto(Produto produto) {
+        this.produto = produto;
+    }
+    
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        NotaItemProduto that = (NotaItemProduto) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
